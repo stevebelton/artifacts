c9a4d06d8249b60afc25e507825d49ee83bda005
Steve Belton <stevebelton@users.noreply.github.com>
2016-08-17 14:28:41 +0100

    Update install-motors-images.ps1

--- artifacts/motors/install-motors-images.ps1
+++ artifacts/motors/install-motors-images.ps1
@@ -1,2 +1,17 @@
-import-module servermanager
-add-windowsfeature web-server -includeallsubfeature
+Import-Module ServerManager
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+
+$guid=[system.guid]::NewGuid().Guid
+$folder="$env:temp\$guid"
+
+New-Item -Path $folder -ItemType Directory
+
+$zipfilelocal = "$folder\imageserver"
+$zipextracted="$folder\extracted"
+
+Invoke-WebRequest "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.8\Downloads\0\imageserver.zip" -OutFile $zipfilelocal
+
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::ExtractToDirectory($zipfilelocal, $zipextracted)
+
+copy-item $zipextracted\imageserver\* c:\inetpub\wwwroot -Force -Recurse
